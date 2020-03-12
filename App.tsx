53fe19cdc88764af7ff3dd93ec731e034b4936a0
Charlie Jackson <charlie@charliejackson.com>
2020-03-12 17:36:47 +0000

    feat(avatar): ✨ let you drag the avatar
    branch master

--- App.tsx
+++ App.tsx
@@ -1,118 +1,151 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
   StatusBar,
+  Animated,
+  PanResponder,
+  PanResponderInstance,
+  GestureResponderEvent,
+  PanResponderGestureState,
 } from 'react-native';
+import styled from 'styled-components/native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const avatarSize = 30;
+const avatarBoundsSize = 120;
 
-declare var global: {HermesInternal: null | {}};
+const avatarCenterOffset = avatarBoundsSize / 2 - avatarSize / 2;
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const bounds = {
+  x: {
+    min: -avatarCenterOffset,
+    max: avatarCenterOffset,
+  },
+  y: {
+    min: -avatarCenterOffset,
+    max: avatarCenterOffset,
+  },
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Avatar = styled(Animated.View)`
+  height: ${avatarSize}px;
+  width: ${avatarSize}px;
+  background-color: black;
+  position: absolute;
+  top: ${-avatarSize / 2}px;
+  left: ${-avatarSize / 2}px;
+`;
+
+const AvatarCenter = styled.View`
+  width: 1px;
+  height: 1px;
+  position: relative;
+`;
+
+const AvatarBounds = styled.View`
+  width: ${avatarBoundsSize}px;
+  height: ${avatarBoundsSize}px;
+  position: relative;
+  background-color: #fafafa;
+`;
+
+const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+interface Props {}
+
+class App extends Component<Props> {
+  private panResponder: PanResponderInstance;
+  private pan: Animated.ValueXY;
+  private default: {x: number; y: number};
+  private reset: null | Animated.CompositeAnimation;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.default = {x: 0, y: 0};
+
+    this.pan = new Animated.ValueXY(this.default);
+
+    const transformGesture = (
+      event: GestureResponderEvent,
+      gestureEvent: PanResponderGestureState,
+    ) => {
+      let x = gestureEvent.dx;
+      let y = gestureEvent.dy;
+
+      if (x > bounds.x.max) {
+        x = bounds.x.max;
+      } else if (x < bounds.x.min) {
+        x = bounds.x.min;
+      }
+
+      if (y > bounds.y.max) {
+        y = bounds.y.max;
+      } else if (y < bounds.y.min) {
+        y = bounds.y.min;
+      }
+
+      return {x, y};
+    };
+
+    this.reset = null;
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderRelease: () => {
+        this.reset = Animated.spring(this.pan, {
+          toValue: this.default,
+          friction: 100,
+        });
+
+        this.reset.start(() => {
+          this.reset = null;
+        });
+      },
+      onPanResponderStart: () => {
+        if (this.reset) {
+          this.reset.stop();
+          this.reset = null;
+        }
+      },
+      onPanResponderMove: (event, gestureEvent) => {
+        this.pan.setValue(transformGesture(event, gestureEvent));
+      },
+    });
+  }
+
+  render() {
+    const panStyle = {
+      transform: this.pan.getTranslateTransform(),
+    };
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <Container {...this.panResponder.panHandlers} style={{zIndex: 2}}>
+            <AvatarCenter>
+              <Avatar style={[panStyle]} />
+            </AvatarCenter>
+          </Container>
+          <Container style={{zIndex: 1}}>
+            <AvatarBounds />
+          </Container>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 export default App;
